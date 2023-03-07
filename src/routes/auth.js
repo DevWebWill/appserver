00ab512ba619100ea8dd72be794b15d29830c296
William Rodríguez <williamrf0402@gmail.com>
2023-03-07 21:46:53 +0100

    pruebas google auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 import verifyJWT from "./verifyjwt.js";
-import { isUserAuth, loginAuth, loginWithGoogle, registerAuth } from "../controllers/Auth.controller.js";
+import { getLoginWithGoogle, isUserAuth, loginAuth, loginWithGoogle, registerAuth } from "../controllers/Auth.controller.js";
 
 const router = express.Router();
 
@@ -11,5 +11,7 @@ router.post('/login', loginAuth);
 router.get("/isuserauth", verifyJWT, isUserAuth)
 
 router.post("/loginwithgoogle", loginWithGoogle)
+
+router.get("/loginwithgoogle", getLoginWithGoogle)
 
 export default router;
